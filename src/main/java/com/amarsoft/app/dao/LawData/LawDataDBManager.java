36d83c3589a0b871548e551540d0eff8401aa487
Ekimin <heyiminwork@gmail.com>
2017-01-11 11:07:44 +0800

    'temp111'

--- src/main/java/com/amarsoft/app/dao/LawData/LawDataDBManager.java
+++ src/main/java/com/amarsoft/app/dao/LawData/LawDataDBManager.java
@@ -3,15 +3,17 @@ package com.amarsoft.app.dao.LawData;
 import com.amarsoft.app.dao.common.MonitorDao;
 import com.amarsoft.app.model.MonitorModel;
 
+import java.sql.Connection;
 import java.util.List;
 
 /**
  * Created by ymhe on 2017/1/10.
  * MonitorSpiderSync
  */
-public class LawDataDBManager implements MonitorDao{
+public class LawDataDBManager implements MonitorDao {
     /**
      * 获取机构号
+     *
      * @param flowId Azkaban的flowId
      * @return
      */
@@ -20,15 +22,44 @@ public class LawDataDBManager implements MonitorDao{
         return null;
     }
 
+    /**
+     * 初始化监控任务
+     *
+     * @param flowId
+     */
     public void initMonitor(String flowId) {
 
     }
 
-    public void initSpiderTask(List<MonitorModel> monitorModelList,String flowId) {
+    /**
+     * 初始化爬虫任务（生成任务）
+     *
+     * @param monitorModelList 监控名单
+     * @param flowId
+     */
+    public void initSpiderTask(List<MonitorModel> monitorModelList, String flowId) {
+        Connection conn = null;
 
     }
 
-    public void insertMonitorList(List<MonitorModel> monitorModelList) {
+    /**
+     * 监控爬虫任务是否完成
+     * <li>企业名单中优先级1和2的任务完成就算完成</li>
+     *
+     * @param monitorModelList
+     * @param flowId
+     */
+    public void monitorSpiderTask(List<MonitorModel> monitorModelList, String flowId) {
+
+    }
+
+    /**
+     * 监控同步任务是否完成
+     *
+     * @param monitorModelList
+     * @param flowId
+     */
+    public void monitorSyncTask(List<MonitorModel> monitorModelList, String flowId) {
 
     }
 }
--- src/main/java/com/amarsoft/app/job/LawDataJob.java
+++ src/main/java/com/amarsoft/app/job/LawDataJob.java
@@ -1,6 +1,11 @@
 package com.amarsoft.app.job;
 
 import com.amarsoft.app.dao.LawData.LawDataDBManager;
+import com.amarsoft.app.dao.MonitorUniMethod;
+import com.amarsoft.app.model.MonitorModel;
+import com.amarsoft.are.ARE;
+
+import java.util.List;
 
 /**
  * Created by ymhe on 2017/1/10.
@@ -8,8 +13,32 @@ import com.amarsoft.app.dao.LawData.LawDataDBManager;
  */
 public class LawDataJob implements MonitorJob{
 
+    /**
+     * 监控爬虫同步程序是否跑完任务
+     * @param flowId
+     */
     public void monitorSpiderSync(String flowId) {
+        String sleepTime = ARE.getProperty("SLEEP_TIME");
+        boolean isSpidered = false;
+        boolean isSynchronized = false;
 
+        List<MonitorModel> monitorModelList = null; //监控名单
+        MonitorUniMethod monitorUniMethod = new MonitorUniMethod();
+        //根据flowId获取相关机构信息
+        LawDataDBManager dbManager = new LawDataDBManager();
+        String bankId = dbManager.getBankIdByFlowId(flowId); //机构号
+
+        monitorModelList = monitorUniMethod.getEntMonitorUrl(bankId); //TODO:获取监控名单
+
+        //生成任务
+        dbManager.initSpiderTask(monitorModelList, flowId);
+
+        //监控任务
+        dbManager.initMonitor(flowId); //生成监控任务
+        while(true){
+            dbManager.monitorSpiderTask(monitorModelList, flowId);//监控爬虫任务
+            dbManager.monitorSyncTask(monitorModelList, flowId); //监控同步任务
+        }
     }
 
     /**
@@ -17,14 +46,7 @@ public class LawDataJob implements MonitorJob{
      * @param flowId
      */
     public void run(String flowId) {
-        //根据flowId获取相关机构信息
-        LawDataDBManager dbManager = new LawDataDBManager();
-        String bankId = dbManager.getBankIdByFlowId(flowId); //机构号
-        //TODO:List<MonitorModel> monitorModelList = MonitorUniMethod.getEntMonitorUrl(bankId); //获取监控名单
-
-        //生成任务
 
 
-        //监控任务
     }
 }
